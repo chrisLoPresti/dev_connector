d1756df3ce45543fb748a22389c234050b01c781
Chris LoPresti <lopresti.chris@yahoo.com>
2018-09-17 12:26:12 -0400

    added keys

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-const uuid = require("uuid");
-module.exports = {
-  //key to connect to mongoDB
-  mongoURI: "mongodb://chris:Clopresti21@ds251632.mlab.com:51632/dev-connector",
-  //secret or key for json web token
-  secretOrKey: uuid()
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
